107cdacb2fa4d8359e17b60f4c14737d37a431b1
coding4startups <56669693+coding4startups@users.noreply.github.com>
2019-12-23 06:37:31 +0100

    added travis file2

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
@@ -9,8 +10,3 @@ test('renders learn react link', () => {
 });
 
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
